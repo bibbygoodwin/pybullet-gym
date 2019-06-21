7219a17022363f261dfedbf88385b858cd0de619
Walter Goodwin <bibbygoodwin@gmail.com>
2019-06-21 02:11:42 +0100

    Implemented phase based envs for roboschool

---
+++ pybulletgym/envs/roboschool/robots/locomotors/phase_based_robots.py
@@ -0,0 +1,115 @@
+from pybulletgym.envs.roboschool.robots.locomotors.walker_base import WalkerBase
+from pybulletgym.envs.roboschool.robots.robot_bases import MJCFBasedRobot
+
+
+class AntPhase(WalkerBase, MJCFBasedRobot):
+    foot_list = ['front_left_foot', 'front_right_foot', 'left_back_foot', 'right_back_foot']
+
+    def __init__(self):
+        WalkerBase.__init__(self, power=2.5)
+        MJCFBasedRobot.__init__(self, "ant.xml", "torso", action_dim=8, obs_dim=28)
+
+    def alive_bonus(self, z, pitch):
+        return +1 if z > 0.26 else -1  # 0.25 is central sphere rad, die if it scrapes the ground
+
+
+
+class HalfCheetahPhase(WalkerBase, MJCFBasedRobot):
+    foot_list = ["ffoot", "fshin", "fthigh",  "bfoot", "bshin", "bthigh"]  # track these contacts with ground
+
+    def __init__(self):
+        WalkerBase.__init__(self, power=0.90)
+        MJCFBasedRobot.__init__(self, "half_cheetah.xml", "torso", action_dim=6, obs_dim=26)
+
+    def alive_bonus(self, z, pitch):
+        # Use contact other than feet to terminate episode: due to a lot of strange walks using knees
+        return +1 if np.abs(pitch) < 1.0 and not self.feet_contact[1] and not self.feet_contact[2] and not self.feet_contact[4] and not self.feet_contact[5] else -1
+
+    def robot_specific_reset(self, bullet_client):
+        WalkerBase.robot_specific_reset(self, bullet_client)
+        self.jdict["bthigh"].power_coef = 120.0
+        self.jdict["bshin"].power_coef  = 90.0
+        self.jdict["bfoot"].power_coef  = 60.0
+        self.jdict["fthigh"].power_coef = 140.0
+        self.jdict["fshin"].power_coef  = 60.0
+        self.jdict["ffoot"].power_coef  = 30.0
+
+class HopperPhase(WalkerBase, MJCFBasedRobot):
+    foot_list = ["foot"]
+
+    def __init__(self):
+        WalkerBase.__init__(self, power=0.75)
+        MJCFBasedRobot.__init__(self, "hopper.xml", "torso", action_dim=3, obs_dim=15)
+
+    def alive_bonus(self, z, pitch):
+        return +1 if z > 0.8 and abs(pitch) < 1.0 else -1
+
+
+class HumanoidPhase(WalkerBase, MJCFBasedRobot):
+    self_collision = True
+    foot_list = ["right_foot", "left_foot"]  # "left_hand", "right_hand"
+
+    def __init__(self, random_yaw = False, random_lean=False):
+        WalkerBase.__init__(self, power=0.41)
+        MJCFBasedRobot.__init__(self, 'humanoid_symmetric.xml', 'torso', action_dim=17, obs_dim=44)
+        # 17 joints, 4 of them important for walking (hip, knee), others may as well be turned off, 17/4 = 4.25
+        self.random_yaw = random_yaw
+        self.random_lean = random_lean
+
+    def robot_specific_reset(self, bullet_client):
+        WalkerBase.robot_specific_reset(self, bullet_client)
+        self.motor_names  = ["abdomen_z", "abdomen_y", "abdomen_x"]
+        self.motor_power  = [100, 100, 100]
+        self.motor_names += ["right_hip_x", "right_hip_z", "right_hip_y", "right_knee"]
+        self.motor_power += [100, 100, 300, 200]
+        self.motor_names += ["left_hip_x", "left_hip_z", "left_hip_y", "left_knee"]
+        self.motor_power += [100, 100, 300, 200]
+        self.motor_names += ["right_shoulder1", "right_shoulder2", "right_elbow"]
+        self.motor_power += [75, 75, 75]
+        self.motor_names += ["left_shoulder1", "left_shoulder2", "left_elbow"]
+        self.motor_power += [75, 75, 75]
+        self.motors = [self.jdict[n] for n in self.motor_names]
+        if self.random_yaw:
+            position = [0,0,0]
+            orientation = [0,0,0]
+            yaw = self.np_random.uniform(low=-3.14, high=3.14)
+            if self.random_lean and self.np_random.randint(2) == 0:
+                if self.np_random.randint(2) == 0:
+                    pitch = np.pi/2
+                    position = [0, 0, 0.45]
+                else:
+                    pitch = np.pi*3/2
+                    position = [0, 0, 0.25]
+                roll = 0
+                orientation = [roll, pitch, yaw]
+            else:
+                position = [0, 0, 1.4]
+                orientation = [0, 0, yaw]  # just face random direction, but stay straight otherwise
+            self.robot_body.reset_position(position)
+            self.robot_body.reset_orientation(p.getQuaternionFromEuler(orientation))
+        self.initial_z = 0.8
+
+    def apply_action(self, a):
+        assert(np.isfinite(a).all())
+        force_gain = 1
+        for i, m, power in zip(range(17), self.motors, self.motor_power):
+            m.set_motor_torque(float(force_gain * power * self.power * np.clip(a[i], -1, +1)))
+
+    def alive_bonus(self, z, pitch):
+        return +2 if z > 0.78 else -1   # 2 here because 17 joints produce a lot of electricity cost just from policy noise, living must be better than dying
+
+
+class Walker2DPhase(WalkerBase, MJCFBasedRobot):
+    foot_list = ["foot", "foot_left"]
+
+    def __init__(self):
+        WalkerBase.__init__(self, power=0.40)
+        MJCFBasedRobot.__init__(self, "walker2d.xml", "torso", action_dim=6, obs_dim=22)
+
+    def alive_bonus(self, z, pitch):
+        return +1 if z > 0.8 and abs(pitch) < 1.0 else -1
+
+    def robot_specific_reset(self, bullet_client):
+        WalkerBase.robot_specific_reset(self, bullet_client)
+        for n in ["foot_joint", "foot_left_joint"]:
+            self.jdict[n].power_coef = 30.0
